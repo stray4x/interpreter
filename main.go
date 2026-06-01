41a8e7b770c9c3f80a004c8d371533f0c191b243
Ivan Chernykh <ivanchernykh98@gmail.com>
2026-06-01 21:52:30 +0300

    feat: lexer

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/stray4x/interpreter/repl"
+)
+
+func main() {
+	fmt.Println("Welcome to the Shreks programming language!")
+	repl.Start(os.Stdin, os.Stdout)
+}
